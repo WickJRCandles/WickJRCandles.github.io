2abeb99e9bbc01a8f088dd2b13840582da2f209b
Steven Sarkisian <ssarkisian@adaction.com>
2024-01-03 16:05:36 -0500

    updates css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+      "./src/**/*.{html,js}",
+      "./index.html",
+  ],
   theme: {
     extend: {},
   },
